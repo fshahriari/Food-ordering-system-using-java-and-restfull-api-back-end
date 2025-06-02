5ac5b45c74250e3cb252cb03e92d1b9c3ca621d6
fshahriari <shahriari.fatemeh2005@gmail.com>
2025-06-02 22:51:19 +0330

    Refactor User class structure and formatting for improved readability

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,2 +1,20 @@
-public class User {
+public class User
+{
+    private String name;
+    private final String phoneNumber;
+    private final String email; //optional
+    private final String password;
+    private final String address; //optional
+    public String id; // =phoneNumber;
+    //BankInfo, for seller, buyer, and courier
+    
+    public User (String name, String phoneNumber, String email,
+         String password, String address)
+    {
+        this.address = address;
+		this.id = phoneNumber;
+		this.phoneNumber = phoneNumber;
+		this.password = password;
+		this.email = email;
+    }
 }
