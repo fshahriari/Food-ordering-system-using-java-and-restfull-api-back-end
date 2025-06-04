03e73aaa295f2b9ceff8ebc35582e1135d4458b4
Zahra Sheikhi <sheikhifatemeh8@gmail.com>
2025-06-04 23:09:45 +0330

    updating Model package

---
+++ src/main/java/com/snappfood/model/BankInfo.java
@@ -0,0 +1,26 @@
+package com.snappfood.model;
+
+public class BankInfo {
+    private String bankName;
+    private String accountNumber;
+
+    public BankInfo() {}
+
+    public BankInfo(String bankName, String accountNumber) {
+        this.bankName = bankName;
+        this.accountNumber = accountNumber;
+    }
+    public String getBankName() {
+        return bankName;
+    }
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+}
--- src/main/java/com/snappfood/model/User.java
+++ src/main/java/com/snappfood/model/User.java
@@ -36,11 +36,41 @@ public class User
     public void setId(int id){
         this.id = id;
     }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+    public String getRole(){
+        return role;
+    }
+    public void setRole(String role){
+        this.role = role;
+    }
     public String getAddress() {
         return address;
     }
     public void setAddress(String address) {
-
+        this.address = address;
     }
     //methods to add: sign up, log in, managing profile(edit, view, delete)
 }
